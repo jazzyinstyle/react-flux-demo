bec72af30305e43b2b94c17d5173987312ffe7dd
Andrew Leong <andrewleong@8common.com>
2019-01-04 21:10:21 +1100

    Enabled all emitChange() call

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -50851,23 +50851,23 @@ Dispatcher.register(function(action) {
   switch(action.actionType) {
     case ActionTypes.INITIALIZE:
       _authors = action.initialData.authors;
-      // AuthorStore.emitChange();
+      AuthorStore.emitChange();
       break;
     case ActionTypes.CREATE_AUTHOR:
       _authors.push(action.author);
-      //AuthorStore.emitChange();
+      AuthorStore.emitChange();
       break;      
     case ActionTypes.UPDATE_AUTHOR:
       const existingAuthor = _.find(_authors, {id: action.author.id});
       const existingAuthorIndex = _.indexOf(_authors, existingAuthor);
       _authors.splice(existingAuthorIndex, 1, action.author);      
-      // AuthorStore.emitChange();
+       AuthorStore.emitChange();
       break; 
     case ActionTypes.DELETE_AUTHOR:
       _.remove(_authors, function(author) {
         return action.id === author.id;
       });
-      // AuthorStore.emitChange();
+      AuthorStore.emitChange();
       break;
     default:
       // no op      
